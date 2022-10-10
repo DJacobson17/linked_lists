27459bdfde95bdc407fd8ab54c7510847991189a
DJacobson17 <davidbjacobson@gmail.com>
2022-10-10 15:59:46 -0500

    finished contains?, find, to_s, and insert_at methods

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -7,22 +7,19 @@ class LinkedList # rubocop:disable Style/Documentation
 
   def initialize
     @head = nil
-    @tail = nil
   end
 
-  def append(value)
-    new_node = Node.new(value)
-    if !@tail
-      @tail = new_node
-    else
-      @tail.next_node = new_node
-    end
+  def append(value, tail = @tail)
+    new_node = Node.new(value, nil)
+    tail.next_node = new_node unless tail.nil?
     @tail = new_node
+    @head ||= new_node
   end
 
   def prepend(value)
     new_node = Node.new(value, @head)
     @head = new_node
+    @tail ||= new_node
   end
 
   def size(current = @head, counter = 1)
@@ -39,9 +36,9 @@ class LinkedList # rubocop:disable Style/Documentation
 
   def pop
     current_node = @head
-    current_node = current_node.next_node until curent_node.next_node == @tail
+    current_node = current_node.next_node until current_node.next_node == @tail
     current_node.next_node = nil
-    @tail = curent_node
+    @tail = current_node
   end
 
   def contains?(value)
@@ -53,9 +50,44 @@ class LinkedList # rubocop:disable Style/Documentation
     end
     current_node.value == value
   end
+
+  def find(value, current_node = @head, index = 0)
+    until current_node == @tail
+      return index if current_node.value == value
+
+      current_node = current_node.next_node
+      index += 1
+    end
+    nil
+  end
+
+  def to_s(current_node = @head, string = '')
+    until current_node == nil
+      string += "( #{current_node.value} ) -> "
+      current_node = current_node.next_node
+    end
+    "#{string} nil"
+  end
+
+  def insert_at(value, index, current_node = @head, current_index = 1)
+    if index.zero?
+      prepend(value)
+    else
+      until current_index == index
+        current_node = current_node.next_node
+        current_index += 1
+      end
+      new_node = Node.new(value, current_node.next_node)
+      current_node.next_node = new_node
+    end
+  end
+
 end
 
 list = LinkedList.new
 list.append(5)
-list.append(7)
-list.append(1)
+list.prepend(7)
+list.prepend(9)
+list.append(23)
+list.insert_at(20, 2)
+puts list.to_s
